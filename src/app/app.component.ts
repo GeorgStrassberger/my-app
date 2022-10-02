0b1b81bb544d951b333e5337e7462109585b197e
Georg <georg.strassberger@googlemail.com>
2022-10-02 20:48:58 +0200

    startDiashow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
 // Hier kommt mein Code rein
+  images = ['1.png','2.jpg','3.jpg', '4.jpg'];
+  currentImage = 0;
 
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval( ()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    },8000);
+  }
 
 
 }
