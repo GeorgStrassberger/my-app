deac856daad258953c7b8ab2758a0314d7a7760c
Georg <georg.strassberger@googlemail.com>
2022-10-02 21:23:05 +0200

    logicDiashow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent implements OnInit {
 // Hier kommt mein Code rein
   images = ['1.png','2.jpg','3.jpg', '4.jpg'];
   currentImage = 0;
+  showImage = true;
 
   ngOnInit(){
     this.updateImage();
@@ -18,6 +19,11 @@ export class AppComponent implements OnInit {
     setInterval( ()=>{
       this.currentImage++;
       this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
     },8000);
   }
 
