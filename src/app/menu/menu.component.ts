fa621c1acc8a5618da5cf7f96feafd9551ea138b
Georg <georg.strassberger@googlemail.com>
2022-10-03 14:33:41 +0200

    darkMode with router & if/else

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  @Input() darkMode = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
